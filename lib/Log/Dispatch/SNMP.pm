0f206211eec75cf2bd2d2fc80812047d973d07e9
Breno G. de Oliveira <garu@cpan.org>
2010-06-10 09:55:49 +0000

    Import of GARU/Log-Dispatch-SNMP-0.02 from CPAN.
    gitpan-cpan-distribution: Log-Dispatch-SNMP
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GARU/Log-Dispatch-SNMP-0.02.tar.gz
    gitpan-cpan-author:       GARU
    gitpan-cpan-maturity:     released

--- lib/Log/Dispatch/SNMP.pm
+++ lib/Log/Dispatch/SNMP.pm
@@ -10,32 +10,50 @@ use Net::SNMP qw(:asn1);
 use Params::Validate qw(validate SCALAR);
 use Carp qw(carp croak); #TODO: option to warn or die or do nothing on errors
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
-sub new
-{
+sub new {
     my $proto = shift;
     my $class = ref $proto || $proto;
 
-    my %p = @_;
+    # new versions of Log::Dispatch may complain about unknown options,
+    # so we take our options out of the equation (err.. hash)
+    my %parameters = @_;
+    my %snmp_parameters = ();
+    foreach my $key (qw(ManagementHostTrapListenPort
+                        ManagementHost
+                        EnterpriseOID
+                        LocalIPAddress
+                        LocalTrapSendPort
+                        GenericTrapType
+                        SpecificTrapType
+                        ApplicationTrapOID
+                        CommunityString
+                     )
+    ) {
+        if (exists $parameters{$key}) {
+            $snmp_parameters{$key} = $parameters{$key};
+            delete $parameters{$key};
+        }
+    }
 
     my $self = bless {}, $class;
 
-    $self->_basic_init(%p);
-    $self->_set_parameters(%p);
+    $self->_basic_init(%parameters);
+    $self->_set_snmp_parameters(%snmp_parameters);
     $self->_start_snmp_session();
     
     return $self;
 }
 
-sub _set_parameters {
+sub _set_snmp_parameters {
     my $self = shift;
 
     # validate user supplied parameters
     my %p = validate (@_, {
                 'ManagementHostTrapListenPort' => { type    => SCALAR,
-                                                  default => 162,
-                                                },
+                                                    default => 162,
+                                                  },
                 'ManagementHost'     => { type => SCALAR },
                 'EnterpriseOID'      => { type => SCALAR },
                 'LocalIPAddress'     => { type => SCALAR },
@@ -62,20 +80,20 @@ sub _start_snmp_session {
     my $self = shift;
     my $error = '';
     
-    ($self->{'session'}, $error) = Net::SNMP->session(
-                                        -hostname    => $self->{'ManagementHost'},
-                                        -port        => $self->{'ManagementHostTrapListenPort'},
-                                        -version     => 'snmpv1', #TODO: make this configurable
-                                        -community   => $self->{'CommunityString'},
-                                        -nonblocking => 0,        #TODO: make this configurable
-                                        -domain      => 'udp4',   #TODO: make this configurable
+    ($self->{'session'}, $error) =
+        Net::SNMP->session(
+               -hostname    => $self->{'ManagementHost'},
+               -port        => $self->{'ManagementHostTrapListenPort'},
+               -version     => 'snmpv1', #TODO: make this configurable
+               -community   => $self->{'CommunityString'},
+               -nonblocking => 0,        #TODO: make this configurable
+               -domain      => 'udp4',   #TODO: make this configurable
     );
     croak "error starting SNMP session: $error\n"
         unless $self->{'session'};
 }
 
-sub log_message
-{
+sub log_message {
     my $self = shift;
     my %p = @_;
 
@@ -95,7 +113,10 @@ sub log_message
     # this is separate in order to easen the future
     # (planned) integration with a special Layout
     # "SnmpDelimitedConversionPatternLayout" for Log4perl.
-    push @varbindlist, $self->{'ApplicationTrapOID'}, OCTET_STRING, $p{'message'};
+    push @varbindlist, $self->{'ApplicationTrapOID'},
+                       OCTET_STRING,
+                       $p{'message'}
+    ;
     
     if ( ! $self->{'session'}->trap(
                                 -enterprise   => $self->{'EnterpriseOID'},
@@ -123,7 +144,7 @@ Log::Dispatch::SNMP - Object for logging to SNMP servers
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 SYNOPSIS
 
@@ -131,14 +152,20 @@ If you're using Log::Dispatch:
 
     use Log::Dispatch::SNMP;
 
-    # all parameters are *required*, except for
-    # ManagementHostTrapListenPort (which defaults to 162),
-    # LocalTrapSendPort (161), CommunityString ('public')
-    # and GenericTrapType (6).
+    # *OPTIONAL* parameters (and their default values)
+    # name                         - defaults to a unique one
+    # max_level                    - defaults to no maximum
+    # ManagementHostTrapListenPort - defaults to 162
+    # LocalTrapSendPort            - defaults to 161
+    # CommunityString              - defaults to 'public'
+    # GenericTrapType              - defaults to 6
+    #
+    # other parameters are *REQUIRED*
     
     my $snmp = Log::Dispatch::SNMP->new(
                  name      => 'snmp',
                  min_level => 'debug',
+                 max_level => 'error',
                  
                  ManagementHost               => '192.168.0.1',
                  ManagementHostTrapListenPort => 162,
@@ -184,7 +211,7 @@ You can also share the same Log4j configuration file of any Java-based applicati
 
 =head1 DESCRIPTION
 
-Log::Dispatch::SNMP is an appender to send logging messages to a specified management host in SNMP-managed networks (with a MLM or SMNP management console or some kind), commonly found in large and/or distributed networks. It should be used under the L<< Log::Dispatch >> system or other compatible logging environments such as L<< Log::Log4perl >>.
+Log::Dispatch::SNMP is an appender to send logging messages to a specified management host in SNMP-managed networks (with a MLM or SMNP management console of some kind), commonly found in large and/or distributed networks. It should be used under the L<< Log::Dispatch >> system or other compatible logging environments such as L<< Log::Log4perl >>.
 
 Note that this appender does not attempt to provide full access to the SNMP API, so you cannot use it as an interface to SNMP GET or SET calls (you should check L<Net::SNMP> and others for this). All we do is pass your logging event as a TRAP.
 
@@ -201,47 +228,49 @@ Instantiates a new logging object. It takes the following parameters (all are re
 
 =over 4
 
-=item name 
+=item name  (optional)
 
-A string containing the name of the object.
+A string containing the name of the logging object. This is useful if you want to refer to the object later, e.g. to log specifically to it or remove it (Log4perl users don't need this at all, as specific logs can be easily done via its configuration).
 
-=item min_level
+By default a unique name will be generated. You should not depend on the form of generated names, as they may change.
 
-A string or integer containing the minimum logging level this object will accept. Please refer to the L<Log::Dispatcher> documentation for further information.
+=item min_level  *REQUIRED*
 
-=item ManagementHost
+A string ('warning') or integer ('3') containing the minimum logging level this object will accept. Please refer to the L<Log::Dispatcher> documentation for further information.
+
+=item ManagementHost  *REQUIRED*
 
 A string containing the SNMP server host name (i.e. the destination for your traps)
 
-=item ManagementHostTrapListenPort
+=item ManagementHostTrapListenPort  (optional)
 
 The port number the SNMP server is listening to. This parameter is optional, and defaults to port 162.
 
-=item EnterpriseOID
+=item EnterpriseOID  *REQUIRED*
 
 A string containing the Enterprise Object Identifier (EOID).
 
-=item LocalIPAddress
+=item LocalIPAddress  *REQUIRED*
 
 A string containing the local IP address.
 
-=item LocalTrapSendPort
+=item LocalTrapSendPort  (optional)
 
 The port number to use locally. This parameter is optional, and defaults to port 161.
 
-=item GenericTrapType
+=item GenericTrapType  (optional)
 
 A number with the Generic Trap type. This parameter is optional, and defaults to 6 which corresponds to "enterpriseSpecific" (what you usually want under message logging).
 
-=item SpecificTrapType
+=item SpecificTrapType  *REQUIRED*
 
 A number with the specific Trap type.
 
-=item ApplicationTrapOID
+=item ApplicationTrapOID  *REQUIRED*
 
 A string containing the Object Identifier (OID) for the Application.
 
-=item CommunityString
+=item CommunityString  (optional)
 
 A string containing the SNMP community name. This parameter is optional, and defaults to 'public'.
 
@@ -312,7 +341,7 @@ L<Net::SNMP>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Breno G. de Oliveira, all rights reserved.
+Copyright 2009-2010 Breno G. de Oliveira, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
